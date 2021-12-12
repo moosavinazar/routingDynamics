b2f54d3cfd433d37a280c414eb12627f8607b67c
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-12 18:52:22 +0330

    create routing with separate routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {UsersComponent} from "./users/users.component";
+import {AdminsComponent} from "./admins/admins.component";
+import {ClientsComponent} from "./clients/clients.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'admins', component: AdminsComponent},
+  { path: 'clients', component: ClientsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
