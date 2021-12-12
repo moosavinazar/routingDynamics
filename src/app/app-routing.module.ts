0432eacd9a538caa5626796f8c82c9a084967e29
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-12 18:58:42 +0330

    add not found path to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {HomeComponent} from "./home/home.component";
 import {UsersComponent} from "./users/users.component";
 import {AdminsComponent} from "./admins/admins.component";
 import {ClientsComponent} from "./clients/clients.component";
+import {NotfoundComponent} from "./notfound/notfound.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
   { path: 'admins', component: AdminsComponent},
-  { path: 'clients', component: ClientsComponent}
+  { path: 'clients', component: ClientsComponent},
+  { path: 'not-found', component: NotfoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
